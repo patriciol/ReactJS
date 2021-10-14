505154c24ca40ae08f6db5e94b6fb5d64f72a448
Patricio Lauria <pato.lauria@hotmail.com>
2021-10-13 21:58:37 -0300

    Entregable 2

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import '../NavBar.css'
+import Button from 'react-bootstrap/Button';
+
+export default function NavBar() {
+    return (
+
+        <div className="contenedor">
+
+            <div><h5 className="titulo">Acuarelas</h5></div>
+            <div>
+            <Button variant="primary">Inicio</Button>{' '}
+                <Button variant="primary">Nosotros</Button>{' '}
+                <Button variant="primary">Cuadros</Button>{' '}
+                <Button variant="primary">Locales</Button>{' '}
+                <Button variant="primary">Contacto</Button>{' '}
+            </div>
+        </div>
+    )
+}
+
